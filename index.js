9e8a70682ec5d68ae9bd68a4fa80fbe428f0e217
SenBryd <169017119+SenBryd@users.noreply.github.com>
2024-05-27 17:41:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const { App } = require('@slack/bolt');
-const appLogic = require('./app');
-
 const app = new App({
   token: process.env.SLACK_USER_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-appLogic(app);
+require('./app')(app);
 
 (async () => {
   await app.start(process.env.PORT);
-  console.log('⚡️ Bolt app is running!');
+  console.log('⚡️ Pavilion SCIM app is running!');
 })();
